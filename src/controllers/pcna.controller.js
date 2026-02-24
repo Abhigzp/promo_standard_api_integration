5f310899b84c8c9a7bde016c0ed1ef52de9e0c7a
Abhishek Rai <rssabhishek4@gmail.com>
2026-02-24 11:29:56 +0530

    somefunction need to be chnaged as per logic

--- src/controllers/pcna.controller.js
+++ src/controllers/pcna.controller.js
@@ -8,5 +8,13 @@ async function getProduct(req, res) {
     res.status(500).json({ success: false, message: error.message });
   }
 }
+async function getInventory(req, res) {
+  try {
+    const data = await pcnaService.getInventory();
+    res.json({ success: true, data });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+}
 
-module.exports = { getProduct };
+module.exports = { getProduct, getInventory };
--- src/routes/pcna.routes.js
+++ src/routes/pcna.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/pcna.controller");
 
 router.get("/product", controller.getProduct);
+router.get("/inventory", controller.getInventory);
+
 
 module.exports = router;
